1a49627b5c146f7eed2a0dfd139e57fa88ac5f0b
Valeriia_Nikitenko <icymail194@gmail.com>
2023-12-04 19:49:34 +0200

    1

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,37 @@
+import { Component, ReactNode } from "react";
+
+interface State {
+  counter: number;
+}
+
+interface Props {}
+
+class About extends Component<Props, State>{
+  constructor(props: Props){
+    super(props);
+    this.state = {
+      counter: 0
+    }
+  }
+  increment = () => {
+    this.setState((prevState)=>({
+      counter: prevState.counter +1
+    }))
+  };
+  decrement = () =>{
+    this.setState((prevState)=>({
+      counter: prevState.counter - 1
+    }))
+  }
+  render():ReactNode {
+    return (
+      <>
+      <h2>About Page: {this.state.counter}</h2>
+      <button onClick={this.increment}>+</button>
+      <button onClick={this.decrement}>-</button>
+      </>
+    )
+  }
+}
+
+export default About;
